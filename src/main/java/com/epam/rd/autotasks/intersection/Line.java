3e0a1eb900c949ad07c4dd2831b01790e7fd721b
Anastasiya Sakharuk <sakharuk333@gmail.com>
2022-12-29 12:11:09 +0300

    Line Intersection

--- src/main/java/com/epam/rd/autotasks/intersection/Line.java
+++ src/main/java/com/epam/rd/autotasks/intersection/Line.java
@@ -1,13 +1,23 @@
 package com.epam.rd.autotasks.intersection;
 
 public class Line {
-
+    int k;
+    int b;
     public Line(int k, int b) {
-
+        this.k = k;
+        this.b = b;
     }
 
-    public Point intersection(Line other) {
-        throw new UnsupportedOperationException();
+    public Point intersection(Line line) {
+        if (this.k == line.k && this.b != line.b) {
+            return null;
+        } else if (this.k == line.k && this.b == line.b) {
+            return null;
+        } else {
+            int x = (line.b - this.b) / (this.k - line.k);
+            int y = this.k * ((line.b - this.b) / (this.k - line.k)) + this.b;
+            return new Point(x, y);
+        }
     }
 
 }
